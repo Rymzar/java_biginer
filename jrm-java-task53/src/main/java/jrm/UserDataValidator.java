520be8e181fe40801feb62108fb57da14894ccf1
Nikta Zemlianskikh <rymzar@gmail.com>
2023-05-06 13:35:13 +0300

    lesson 55

--- jrm-java-task53/src/main/java/jrm/UserDataValidator.java
+++ jrm-java-task53/src/main/java/jrm/UserDataValidator.java
@@ -4,10 +4,12 @@ public class UserDataValidator {
     public static void validatePhoneNumber(User user) {
         try{
             if (user.getPhone().length() < 8) {
-                throw new InvalidPhoneLengthException();
+                //генерировать свое исключение
+                throw new IllegalArgumentException ("Телефон не может быть короче 8 символов");
             }
-            if (!user.getPhone().startsWith("7")) {
-                throw new InvalidPhoneFormatException();
+            if (!user.getPhone().startsWith("7")){
+                //генерировать свое исключение
+                throw new IllegalArgumentException ("Номер телефона должен начинаться с цифры 7");
             }
         }catch (NullPointerException e){
             System.out.println("Номер телефона не заполнен");
